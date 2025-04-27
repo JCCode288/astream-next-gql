57ecad1d5f2525fcdf4dadfe72b2bd94bf57d159
jc288 <jendycaprico@gmail.com>
2025-04-27 17:59:10 +0700

    feat(search): basic functionality feat(main): refactor main page into smaller components

---
+++ src/components/main-genres.tsx
@@ -0,0 +1,28 @@
+import { ChevronRight } from "lucide-react";
+import { Button } from "./ui/button";
+import GenreCard from "./main/main-card";
+
+export interface IGenreProps {
+   genres: string[];
+}
+
+export default function MainGenres({ genres }: IGenreProps) {
+   return (
+      <div className="mb-12">
+         <div className="flex items-center justify-between mb-6">
+            <h2 className="text-2xl font-bold">Top Genres</h2>
+            <Button
+               variant="link"
+               className="text-rose-500 hover:text-rose-400 p-0"
+            >
+               Browse All <ChevronRight className="h-4 w-4 ml-1" />
+            </Button>
+         </div>
+         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+            {genres.map((genre, i) => (
+               <GenreCard key={i} genre={genre} index={i} />
+            ))}
+         </div>
+      </div>
+   );
+}
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,9 +3,36 @@ import authStore from "@/lib/stores/auth.store";
 import Link from "next/link";
 import UserMenu from "./auth/user-menu";
 import GoogleButton from "./auth/google-button";
+import { Search } from "lucide-react";
+import { Input } from "./ui/input";
+import animeStore from "@/lib/stores/animes.store";
+import { ChangeEvent, KeyboardEvent } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
    const isLoggedIn = authStore().isLoggedIn;
+   const router = useRouter();
+
+   const query = animeStore().query;
+   const setQuery = animeStore().setQuery;
+
+   const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+      setQuery(e.target?.value);
+   };
+
+   const handleSubmitSearch = (e: KeyboardEvent<HTMLInputElement>) => {
+      const key = e.key;
+
+      if (key !== "Enter") return;
+      if (!query) return;
+
+      router.push("/search");
+   };
+
+   const handleIconTap = () => {
+      if (!query) return;
+      router.push("/search");
+   };
 
    return (
       <header className="sticky top-0 z-50 w-full border-b border-zinc-800 bg-black/95 backdrop-blur supports-[backdrop-filter]:bg-black/60">
@@ -24,21 +51,35 @@ export default function Navbar() {
                   >
                      Home
                   </Link>
-                  <Link
-                     href="/categories"
+                  {/* <Link
+                     href="/genres"
                      className="text-sm font-medium text-zinc-400 transition-colors hover:text-white"
                   >
-                     Categories
-                  </Link>
-                  <Link
+                     Genres
+                  </Link> */}
+                  {/* <Link
                      href="/mylist"
                      className="text-sm font-medium text-zinc-400 transition-colors hover:text-white"
                   >
                      My List
-                  </Link>
+                  </Link> */}
                </nav>
             </div>
             <div className="flex items-center gap-4">
+               <div className="relative hidden md:block w-full max-w-sm">
+                  <Search
+                     onClick={handleIconTap}
+                     className="absolute left-2.5 top-2.5 h-4 w-4 text-zinc-500 cursor-pointer"
+                  />
+                  <Input
+                     type="search"
+                     placeholder="Search anime..."
+                     className="w-full bg-zinc-900 pl-8 text-sm text-white border-zinc-800 focus-visible:ring-rose-500"
+                     onChange={handleSearch}
+                     onKeyDown={handleSubmitSearch}
+                     value={query ?? ""}
+                  />
+               </div>
                {isLoggedIn ? <UserMenu /> : <GoogleButton />}
             </div>
          </div>
