473353ca9c49b31eeecd912eaee3a1360a7225c4
jc288 <jendycaprico@gmail.com>
2025-04-28 20:44:47 +0700

    feat(deployment): move handler to api route and change handler type

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,4 +1,75 @@
-import handler from "@/lib/graphql/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import server from "@/lib/graphql/server";
+import { ProviderEnum } from "@/lib/anime-provider/provider.interfaces";
+import { ApolloError } from "@apollo/client";
 
-export const GET = handler;
-export const POST = handler;
+const handler = startServerAndCreateNextHandler(server, {
+   context: async (request) => {
+      const headers: any = request.headers;
+      const token = headers.get("authorization");
+      let provider = headers.get("x-ani-provider");
+      const origin = headers.get("origin");
+      const referer = headers.get("referer");
+
+      console.log({
+         token,
+         provider,
+         origin,
+         referer,
+         whitelist: process.env.WHITELIST,
+      });
+
+      if (!origin || !referer)
+         throw new ApolloError({
+            networkError: new Error("cannot access"),
+         });
+
+      const refererURL = new URL(referer);
+      const originURL = new URL(origin);
+
+      if (originURL.origin !== process.env.WHITELIST) {
+         throw new ApolloError({
+            networkError: new Error("cannot access"),
+         });
+      }
+
+      if (originURL.origin !== refererURL.origin) {
+         throw new ApolloError({
+            networkError: new Error("cannot access"),
+         });
+      }
+
+      if (!provider) {
+         provider = ProviderEnum.ZORO;
+
+         return {
+            token,
+            provider,
+         };
+      }
+      if (typeof provider !== "string") {
+         provider = ProviderEnum.ZORO;
+
+         return {
+            token,
+            provider,
+         };
+      }
+      if (!Object.values(ProviderEnum).includes(provider as any)) {
+         provider = ProviderEnum.ZORO;
+
+         return {
+            token,
+            provider,
+         };
+      }
+
+      return {
+         token,
+         provider,
+      };
+   },
+});
+
+export const GET = handler as any;
+export const POST = handler as any;
--- src/lib/graphql/server/index.ts
+++ src/lib/graphql/server/index.ts
@@ -1,10 +1,7 @@
 import typeDefs from "./type.defs";
 import resolvers from "./resolvers";
 import { ApolloServer } from "@apollo/server";
-import { startServerAndCreateNextHandler } from "@as-integrations/next";
-import { ProviderEnum } from "@/lib/anime-provider/provider.interfaces";
 import { InMemoryLRUCache } from "@apollo/utils.keyvaluecache";
-import { ApolloError } from "@apollo/client";
 
 const server = new ApolloServer({
    typeDefs,
@@ -16,70 +13,4 @@ const server = new ApolloServer({
    csrfPrevention: true,
 });
 
-export default startServerAndCreateNextHandler(server, {
-   context: async (request) => {
-      const headers: any = request.headers;
-      const token = headers.get("authorization");
-      let provider = headers.get("x-ani-provider");
-      const origin = headers.get("origin");
-      const referer = headers.get("referer");
-
-      console.log({
-         token,
-         provider,
-         origin,
-         referer,
-         whitelist: process.env.WHITELIST,
-      });
-
-      if (!origin || !referer)
-         throw new ApolloError({
-            networkError: new Error("cannot access"),
-         });
-
-      const refererURL = new URL(referer);
-      const originURL = new URL(origin);
-
-      if (originURL.origin !== process.env.WHITELIST) {
-         throw new ApolloError({
-            networkError: new Error("cannot access"),
-         });
-      }
-
-      if (originURL.origin !== refererURL.origin) {
-         throw new ApolloError({
-            networkError: new Error("cannot access"),
-         });
-      }
-
-      if (!provider) {
-         provider = ProviderEnum.ZORO;
-
-         return {
-            token,
-            provider,
-         };
-      }
-      if (typeof provider !== "string") {
-         provider = ProviderEnum.ZORO;
-
-         return {
-            token,
-            provider,
-         };
-      }
-      if (!Object.values(ProviderEnum).includes(provider as any)) {
-         provider = ProviderEnum.ZORO;
-
-         return {
-            token,
-            provider,
-         };
-      }
-
-      return {
-         token,
-         provider,
-      };
-   },
-});
+export default server;
