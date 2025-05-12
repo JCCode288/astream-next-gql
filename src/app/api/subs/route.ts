e3f98e1302f065df3b3d7d981b8b2c377ebc4e7a
jc288 <jendycaprico@gmail.com>
2025-05-12 18:22:14 +0700

    fix(auth): change request validation method

--- src/app/api/subs/route.ts
+++ src/app/api/subs/route.ts
@@ -1,9 +1,10 @@
-import client from "@/lib/database";
-import { Binary } from "bson";
+import { ISubsData } from "@/lib/api/api.interface";
+import APIProvider from "@/lib/api/provider";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
    try {
+      const apiProvider = new APIProvider();
       const query = req.nextUrl.searchParams;
 
       let paramsUrl = query.get("url");
@@ -20,21 +21,17 @@ export async function GET(req: NextRequest) {
       };
       let resHeaders: Record<string, string> = {};
 
-      const subsData = await getFromAPI(filter);
+      const subsData = await apiProvider.getSubs(filter);
 
       if (subsData) {
          console.log("<< DB Hit Subs >>");
-         if (subsData.data instanceof Binary)
-            return new NextResponse(subsData.data?.buffer, {
-               headers: subsData.headers,
-            });
 
          return new NextResponse(subsData.data, {
             headers: subsData.headers,
          });
       }
 
-      console.log("<< DB Miss >>");
+      console.log("<< DB Miss Subs >>");
 
       const res = await fetch(paramsUrl, {
          method: "GET",
@@ -54,10 +51,10 @@ export async function GET(req: NextRequest) {
       const saveSubs: ISubsData = {
          animeId,
          episodeId,
-         data: new Binary(Buffer.from(data)),
+         data: Buffer.from(data).toString("base64"),
          headers: resHeaders,
       };
-      await saveToAPI(saveSubs);
+      await apiProvider.saveSubs(saveSubs);
 
       return new NextResponse(data, {
          headers: resHeaders,
@@ -65,42 +62,5 @@ export async function GET(req: NextRequest) {
    } catch (err) {
       console.log(err);
       throw err;
-   }
-}
-
-// @todo - later refactor the interfaces
-export interface ISubsFilter {
-   animeId: string;
-   episodeId: string;
-}
-
-export interface ISubsData extends ISubsFilter {
-   data: string | Binary; // base64 from buffer subtitle @todo - later change to only string
-   headers: Record<string, string>;
-}
-
-async function getFromAPI(
-   filter: ISubsFilter
-): Promise<ISubsData | undefined> {
-   try {
-      let subsData = await client
-         .collection<ISubsData>("subs")
-         .findOne(filter);
-
-      if (subsData) return subsData;
-
-      return;
-   } catch (err) {
-      console.log("[Failed to retrieve subtitle] ", err);
-      return;
-   }
-}
-
-async function saveToAPI(subsData: ISubsData) {
-   try {
-      const _ = await client.collection("subs").insertOne(subsData);
-   } catch (err) {
-      console.error(err);
-      console.log("[Failed to save stream]");
    }
 }
--- src/lib/api/api.interface.ts
+++ src/lib/api/api.interface.ts
@@ -1,7 +1,26 @@
+import { Binary } from "mongodb";
+
 export interface IStreamData {
    animeId: string;
    episodeId: string;
    segment: string;
    data: string; // Buffer to base64
    headers: Record<string, string>;
+}
+
+export interface ISubsFilter {
+   animeId: string;
+   episodeId: string;
+}
+
+export interface ISubsData extends ISubsFilter {
+   data: string; // Buffer to base64
+   headers: Record<string, string>;
+}
+
+export interface IKeyInterface {
+   animeId?: string;
+   episodeId?: string;
+   segment?: string;
+   data?: string;
 }
