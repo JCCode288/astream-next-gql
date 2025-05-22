cfdfecbdc817bd631d4adf7282272c7c239bc7ef
jc288 <jendycaprico@gmail.com>
2025-05-22 14:09:18 +0700

    chore(unused): remove unused component

--- src/app/anime/[id]/watch/[epsId]/page.tsx
+++ src/app/anime/[id]/watch/[epsId]/page.tsx
@@ -169,25 +169,3 @@ export default function WatchPage() {
       </div>
    );
 }
-
-// Custom Info icon component
-function Info(props: React.SVGProps<SVGSVGElement>) {
-   return (
-      <svg
-         {...props}
-         xmlns="http://www.w3.org/2000/svg"
-         width="24"
-         height="24"
-         viewBox="0 0 24 24"
-         fill="none"
-         stroke="currentColor"
-         strokeWidth="2"
-         strokeLinecap="round"
-         strokeLinejoin="round"
-      >
-         <circle cx="12" cy="12" r="10" />
-         <path d="M12 16v-4" />
-         <path d="M12 8h.01" />
-      </svg>
-   );
-}
